932f62d7ed36e27640e382bf70f8a15ef7465278
jmrojas2332 <jmrojas2332@gmail.com>
2019-09-23 00:47:29 -0500

    Updates to pass tests 1-7

--- shell/wish.c
+++ shell/wish.c
@@ -9,13 +9,15 @@ int main(int argc, char *argv[])
 {
     FILE *inputFile = NULL;
     char *buffer = NULL;
-    char *path = "/bin";
     char error_message[30] = "An error has occurred\n";
-    size_t line_buf_size = 0; 
+    size_t line_buf_size = 0;
+    char *path = malloc((strlen("/bin")) * sizeof(*path));
+    strcpy(path , "/bin");
 
     if (argc > 2)
     {
         write(STDERR_FILENO, error_message, strlen(error_message)); 
+        free(path);
         exit(1);
     }
     else if (argc == 2)
@@ -25,13 +27,17 @@ int main(int argc, char *argv[])
         if(!inputFile)
         {
             write(STDERR_FILENO, error_message, strlen(error_message)); 
+            free(path);
             exit(1);
         }
     }
 
     while (1)
     {
-        printf("wish> ");
+        if (!inputFile)
+        {
+            printf("wish> ");
+        }
 
         if(getline(&buffer, &line_buf_size, inputFile ? inputFile : stdin) == -1) // EOF reached
         {
@@ -41,15 +47,17 @@ int main(int argc, char *argv[])
             }
 
             free(buffer); // from doc: buffer should be freed, even if getline fails (or EOF reached)
+            free(path);
             exit(0);
         }
         
         char *delim = " \t\n\r";
         char *token;
-        char **argv = malloc(sizeof(char*) * 1);
+        char *itr = buffer;
+        char **args = malloc(sizeof(*args));
         int i = 0;
-
-        while((token = strsep(&buffer, delim)) != NULL)
+        
+        while((token = strsep(&itr, delim)) != NULL)
         {
             if (strstr(delim, token)) // do not store delim chars
             {
@@ -58,17 +66,28 @@ int main(int argc, char *argv[])
 
             if (i != 0)
             {
-                argv = realloc(argv, sizeof(char*) * (i + 1)); // reallocate space for new char*
+                args = realloc(args, (i + 1) * sizeof(*args)); // reallocate space of args to include space for new string
             }
             
-            argv[i] = malloc((strlen(token) + 1) * sizeof(token)); // +1 to include null-terminating char
-            argv[i++] = token;
+            args[i] = malloc((strlen(token)) * sizeof(**args));
+            strcpy(args[i], token);
+            i += 1;
         }
 
-        argv[i] = NULL; // marks end of array
-
-        if (!strcmp(argv[0], "exit")) // do not exit until exit is entered
+        if (!strcmp(args[0], "exit")) // do not exit until exit is entered
         {
+            if (i > 1) // we cannot have an arg with exit, first arg is "exit"
+            {
+                write(STDERR_FILENO, error_message, strlen(error_message));
+
+                for (int k = 0; k < i; ++k)
+                {
+                    free(args[k]);
+                }
+                free(args);
+                continue;
+            }
+
             if (inputFile)
             {
                 fclose(inputFile);
@@ -76,23 +95,41 @@ int main(int argc, char *argv[])
 
             for (int k = 0; k < i; ++k)
             {
-                free(argv[k]);
+                free(args[k]);
             }
 
-            free(argv);
+            free(args);
             free(buffer);
+            free(path);
             exit(0);        
         }
-        else if (!strcmp(argv[0], "cd"))
+        else if (!strcmp(args[0], "cd"))
         {
             if (i == 1 || i > 2) // 1st arg is program, but we need exactly 1 more argument to chdir
             {
                 write(STDERR_FILENO, error_message, strlen(error_message));
-                chdir(argv[1]);
+                for (int k = 0; k < i; ++k)
+                {
+                    free(args[k]);
+                }
+                free(args);
+                continue;
             }
+
+            if(chdir(args[1]))
+            {
+                write(STDERR_FILENO, error_message, strlen(error_message));
+            }
+            continue;
         }
-        else if (!strcmp(argv[0], "path"))
+        else if (!strcmp(args[0], "path"))
         {
+            strcpy(path, "");
+            for (int k = 1; k < i; ++k)
+            {
+                path = realloc(path, (strlen(path) + strlen(args[k])) * sizeof(*path));
+                strcat(path, args[k]);
+            }
         }        
         else
         {
@@ -102,27 +139,30 @@ int main(int argc, char *argv[])
             {
                 write(STDERR_FILENO, error_message, strlen(error_message));
             }
-            else if (rc == 0) // child process
-            {
-                printf("child");
-                printf("%s", path);
-                if(execv(strcat(strcat(path, "/"), argv[0]), argv))
-                    write(STDERR_FILENO, error_message, strlen(error_message));
-                exit(0);
-            }
-            else if (rc > 0) // parent process
+            else if (rc > 0) // parent process enters here
             {
                 wait(NULL);
-                printf("parent");
+            }
+            else // child process enters here
+            {
+                char *program = malloc(((strlen(path) + strlen(args[0])) * sizeof(char))); 
+                strcpy(program, path);
+                strcat(strcat(program, "/"), args[0]);
+
+                if(execv(program, args))
+                    write(STDERR_FILENO, error_message, strlen(error_message));
+                free(program);
+                exit(0);
             }
         }
 
         for (int k = 0; k < i; ++k)
         {
-            free(argv[k]);
+            free(args[k]);
         }
 
-        free(argv);
+        free(args);
+        free(buffer);
     }
 
     return 0;
